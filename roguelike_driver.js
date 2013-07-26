2e2f8a39142f5992d50978092528f852916526d8
tonisbones <decausem@gmail.com>
2013-07-26 09:30:40 -0400

    Skeleton of the project

---
+++ roguelike_driver.js
@@ -0,0 +1,6 @@
+var canv = document.getElementById("myCanvas");
+var CANV_WIDTH = canv.getAttribute("width");
+var CANV_HEIGHT = canv.getAttribute("height");
+var ctx = canv.getContext("2d");
+
+ctx.fillRect(0,0,10,10);
